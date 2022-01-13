a25d9c7a3ef22cf17842637e0762f133684db080
robzor92 <robin.eric.andersson@gmail.com>
2022-01-13 16:06:59 +0100

    fix format

--- python/hsml/model.py
+++ python/hsml/model.py
@@ -304,7 +304,9 @@ class Model:
     @property
     def version_path(self):
         """path of the model including version folder. Resolves to /Projects/{project_name}/Models/{name}/{version}"""
-        return "/Projects/{}/Models/{}/{}".format(self.project_name, self.name, str(self.version))
+        return "/Projects/{}/Models/{}/{}".format(
+            self.project_name, self.name, str(self.version)
+        )
 
     @property
     def shared_registry_project_name(self):
