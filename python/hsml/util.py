a71b6f3198334a6b361fe9481736264f94e1a7f6
robzor92 <robin.eric.andersson@gmail.com>
2022-06-16 13:24:42 +0200

    fix

--- python/hsml/util.py
+++ python/hsml/util.py
@@ -180,36 +180,29 @@ def decompress(archive_file_path, extract_dir=None):
 
 
 def validate_metrics(metrics):
-    if not isinstance(metrics, dict):
-        raise TypeError(
-            "provided metrics is of instance {}, expected a dict".format(type(metrics))
-        )
-
-    for metric in metrics:
-        if not isinstance(metric, string_types):
+    if metrics is not None:
+        if not isinstance(metrics, dict):
             raise TypeError(
-                "provided metrics key is of instance {}, expected a string".format(
-                    type(metric)
-                )
+                "provided metrics is of instance {}, expected a dict".format(type(metrics))
             )
-        validate_metric_value(metrics[metric])
 
-
-def validate_metric_value(opt_val):
-    try:
-        int(opt_val)
-        return opt_val
-    except Exception:
-        pass
-    try:
-        float(opt_val)
-        return opt_val
-    except Exception:
-        pass
-    raise TypeError(
-        "Metric value is of type {}, expecting a number".format(type(opt_val))
-    )
-
+        for metric in metrics:
+            # Validate key is a string
+            if not isinstance(metric, string_types):
+                raise TypeError(
+                    "provided metrics key is of instance {}, expected a string".format(
+                        type(metric)
+                    )
+                )
+            # Validate value is a number
+            try:
+                float(metrics[metric])
+            except ValueError:
+                raise ValueError(
+                    "{} is not a number, only numbers can be attached as metadata for models.".format(
+                        str(metrics[metric])
+                    )
+                )
 
 def get_predictor_for_model(model, **kwargs):
     if not isinstance(model, BaseModel):
@@ -290,19 +283,3 @@ def get_hostname_replaced_url(sub_path: str):
     url_parsed = client.get_instance().replace_public_host(urlparse(href))
     return url_parsed.geturl()
 
-
-def _validate_metrics(metrics):
-    if metrics is not None:
-        assert type(metrics) is dict, "provided metrics is not in a dict"
-        for metric in metrics:
-            assert isinstance(
-                metric, string_types
-            ), "metrics key {} is not a string".format(str(metric))
-            try:
-                float(metrics[metric])
-            except ValueError:
-                raise ValueError(
-                    "{} is not a number, only numbers can be attached as metadata for models.".format(
-                        str(metrics[metric])
-                    )
-                )
