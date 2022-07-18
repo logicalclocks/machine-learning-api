f36c329da129c26c4a93b7b0b1fc23910556ad8e
moritzmeister <moritz@logicalclocks.com>
2022-07-18 14:10:06 +0200

    add pymdown extensions dependency

--- python/setup.py
+++ python/setup.py
@@ -53,7 +53,9 @@ setup(
             "mike==1.1.2",
             "sphinx==3.5.4",
             "keras_autodoc @ git+https://git@github.com/logicalclocks/keras-autodoc@split-tags-properties",
-            "markdown-include"]
+            "markdown-include",
+            "pymdown-extensions",
+        ]
     },
     author="Logical Clocks AB",
     author_email="robin@logicalclocks.com",
