8331f47c55a514fd43e3916c625a315dbb43414c
ErmiasG <ermiasg@kth.se>
2022-09-29 09:44:56 +0200

    [HOPSWORKS-3278]-append type not set in DTO (#147)

--- python/hsml/python/model.py
+++ python/hsml/python/model.py
@@ -66,6 +66,5 @@ class Model(Model):
 
     def update_from_response_json(self, json_dict):
         json_decamelized = humps.decamelize(json_dict)
-        _ = json_decamelized.pop("type")
         self.__init__(**json_decamelized)
         return self
