fbf50a15c192e5e4aa672879bdaceb5e546375ab
Fabio Buso <dev.siroibaf@gmail.com>
2024-04-29 10:42:24 +0200

    [HWORKS-1171] Make chunk size configurable when uploading models to t… (#228)
    * [HWORKS-1171] Make chunk size configurable when uploading models to the model registry from outside

--- python/hsml/engine/hopsworks_engine.py
+++ python/hsml/engine/hopsworks_engine.py
@@ -33,7 +33,7 @@ class HopsworksEngine:
         remote_path = self._prepend_project_path(remote_path)
         self._native_hdfs_api.rm(remote_path)
 
-    def upload(self, local_path: str, remote_path: str):
+    def upload(self, local_path: str, remote_path: str, upload_configuration=None):
         local_path = self._get_abs_path(local_path)
         remote_path = self._prepend_project_path(remote_path)
         self._native_hdfs_api.upload(local_path, remote_path)
